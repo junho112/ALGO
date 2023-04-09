a3d9f3ab8fbd758f92c860473d784fb5e2acae30
junho112 <byon26@naver.com>
2023-04-09 11:25:08 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 116 ms, Memory: 13420 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -1,78 +1,61 @@
-
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main{
 	static int damage[][] = {{9,3,1},{9,1,3},{3,9,1},{3,1,9},{1,3,9},{1,9,3}};
-	static int min = Integer.MAX_VALUE;
-	static int INF = Integer.MAX_VALUE/100;
-	static int dp[][][] = new int[61][61][61];
 	public static void main(String[] agrs) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		Integer arr[] = new Integer[3];
-		for(int i=0;i<T;i++) {
+		int N = sc.nextInt();
+		int arr[] = new int[3];
+		for(int i=0;i<N;i++) {
 			arr[i] = sc.nextInt();
 		}
-		for(int i=0;i<61;i++) {
-			for(int k=0;k<61;k++) {
-				for(int j=0;j<61;j++) {
-					dp[i][k][j] = INF;
+		Queue<Node> mq = new ArrayDeque<>();
+		boolean visit[][][] = new boolean[61][61][61];
+		visit[arr[0]][arr[1]][arr[2]] = true;
+		mq.add(new Node(arr[0],arr[1],arr[2],0));
+		while(!mq.isEmpty()) {
+			Node tmp = mq.poll();
+			if(tmp.s1==0&&tmp.s2==0&&tmp.s3==0) {
+				System.out.println(tmp.cnt);
+			}
+			for(int i=0;i<6;i++) {
+				int tmp1 = tmp.s1 - damage[i][0];
+				int tmp2 = tmp.s2 - damage[i][1];
+				int tmp3 = tmp.s3 - damage[i][2];
+				if(tmp1<0) {
+					tmp1 = 0;
 				}
+				if(tmp2<0) {
+					tmp2 = 0;
+				}
+				if(tmp3<0) {
+					tmp3 = 0;
+				}
+				if(visit[tmp1][tmp2][tmp3]) {
+					continue;
+				}
+				visit[tmp1][tmp2][tmp3] = true;
+				mq.add(new Node(tmp1,tmp2,tmp3,tmp.cnt+1));
 			}
 		}
-		for(int i=T;i<3;i++) {
-			arr[i] = 0;
-		}
-//		System.out.println(Arrays.toString(arr));
-		int cnt = dfs(arr.clone(),0);
-		System.out.println(cnt);
+		
 	}
-	/**
-	 * 9 3 1
-	 * 9 1 3
-	 * 3 9 1
-	 * 3 1 9
-	 * 1 3 9
-	 * 1 9 3 
-	 * @param arr
-	 * @param def
-	 * @return
-	 */
-	private static int dfs(Integer[] arr,int def) {
-		Arrays.sort(arr,Collections.reverseOrder());
-		int s1 = arr[0];
-		int s2 = arr[1];
-		int s3 = arr[2];
-		if(s1<=0&&s2<=0&&s3<=0) {
-			return def;
+	
+	static class Node{
+		int s1;
+		int s2;
+		int s3;
+		int cnt;
+		public Node(int s1, int s2, int s3, int cnt) {
+			super();
+			this.s1 = s1;
+			this.s2 = s2;
+			this.s3 = s3;
+			this.cnt = cnt;
 		}
-		if(dp[s1][s2][s3]!=INF) { //들린데임 
-			return dp[s1][s2][s3];
-		}
-		for(int i=0;i<6;i++) {
-			int tmps1 = s1-damage[i][0];
-			if(tmps1<0) {
-				tmps1 = 0;
-			}
-			int tmps2 = s2-damage[i][1];
-			if(tmps2<0) {
-				tmps2 = 0;
-			}
-			int tmps3 = s3-damage[i][2];
-			if(tmps3<0) {
-				tmps3 = 0;
-			}
-			Integer tmparr[] = new Integer[3];
-			tmparr[0] = tmps1;
-			tmparr[1] = tmps2;
-			tmparr[2] = tmps3;
-//			Arrays.sort(tmparr);
-//			System.out.println(tmps1+" "+tmps2+" "+tmps3+" "+def);
-			dp[s1][s2][s3] = Math.min(dp[s1][s2][s3],dfs(tmparr,def+1));
-		}
-		return dp[s1][s2][s3];
+		
 	}
 
 }
