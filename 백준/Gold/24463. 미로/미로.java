a415226ddbc0db343fd4fa7f4824ef52749cb1a5
junho112 <byon26@naver.com>
2023-04-24 20:57:14 +0900

    [Gold IV] Title: 미로, Time: 604 ms, Memory: 73428 KB -BaekjoonHub

--- 백준/Gold/24463. 미로/미로.java
+++ 백준/Gold/24463. 미로/미로.java
@@ -1,30 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main{
 	static int N,M;
 	static char[][] arr;
-	static int endy,endx,starty,startx;
-	static int visit[][];
+	static int min = Integer.MAX_VALUE;
+	static int endy,endx;
+	static boolean visit[][];
 	static char[][] result;
-	static int num;
 	static int dy[] = {-1,1,0,0};
 	static int dx[] = {0,0,-1,1};
 	public static void main(String[] agrs) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		StringBuilder sb = new StringBuilder();
 		arr = new char[N][M];
 		result = new char[N][M];
-		visit = new int[N][M];
-		startx = 0;
-		starty = 0;
+		visit = new boolean[N][M];
+		int startx = 0;
+		int starty = 0;
 		endx = 0;
 		endy = 0;
 		boolean flag = true;
@@ -44,93 +42,58 @@ public class Main{
 				}
 			}
 		}
-		bfs(starty,startx);
-		result[endy][endx] = '.'; 
-		result[starty][startx] = '.'; 
-		bfs2(endy,endx);
+		visit[starty][startx] = true;
+		dfs(starty,startx,0);
 		for(int i=0;i<N;i++) {
 			for(int k=0;k<M;k++) {
-				if(result[i][k]=='.') {
-					sb.append('.');
-//					System.out.print('.');
-				}else if(arr[i][k]=='.') {
-//					System.out.print('@');
-					sb.append('@');
-				}else if(arr[i][k]=='+') {
-//					System.out.print('+');
-					sb.append('+');
-				}
+//				System.out.print(result[i][k]);
+				sb.append(result[i][k]);
 			}
 			sb.append("\n");
 		}
 		System.out.println(sb);
-	}
-	
-	private static void bfs2(int endy2, int endx2) {
-		// TODO Auto-generated method stub
-		Queue<Node> mq = new ArrayDeque<>();
-		mq.add(new Node(endy,endx,num));
-		while(!mq.isEmpty()) {
-			Node tmp = mq.poll();
-			if(tmp.y==starty&&tmp.x==startx) {
-				return;
-			}
-			for(int i=0;i<4;i++) {
-				int cy = dy[i] +tmp.y;
-				int cx = dx[i] +tmp.x;
-				if(cy>=0&&cx>=0&&cy<N&&cx<M) {
-					if(visit[cy][cx]==tmp.cnt-1) {
-						result[cy][cx] = '.';
-						mq.add(new Node(cy,cx,tmp.cnt-1));
-						continue;
-					}
-				}
-			}
-		}
-	}
-
-	private static void bfs(int starty, int startx) {
-		// TODO Auto-generated method stub
-		Queue<Node> mq = new ArrayDeque<>();
-		mq.add(new Node(starty,startx,1)); // 1부터 시작
-		while(!mq.isEmpty()) {
-			Node tmp = mq.poll();
-			if(tmp.y==endy&&tmp.x==endx) {
-				num=tmp.cnt;
-				return;
-			}
-			for(int i=0;i<4;i++) {
-				int cy = dy[i] +tmp.y;
-				int cx = dx[i] +tmp.x;
-				if(cy>=0&&cx>=0&&cy<N&&cx<M) {
-					if(arr[cy][cx]=='.'&&visit[cy][cx]==0) {
-						visit[cy][cx] = tmp.cnt+1;
-						mq.add(new Node(cy,cx,tmp.cnt+1));
-					}
-				}
-			}
-		}
-	}
-
-
-
-
-
-
-
-
-
-
-	static class Node{
-		int y;
-		int x;
-		int cnt;
-		public Node(int y, int x, int cnt) {
-			super();
-			this.y = y;
-			this.x = x;
-			this.cnt = cnt;
-		}
 		
+	}
+	private static void dfs(int y, int x, int def) {
+		// TODO Auto-generated method stub
+//		System.out.println(y+" "+x);
+		if(def>min) {
+			//가지치기
+			return;
+		}
+		if(y==endy&&x==endx) {
+			if(min>def) {
+				min = def;
+				//결과 그림그리기
+//				System.out.println(def);
+				draw();
+			}
+			return;
+		}
+		for(int i=0;i<4;i++) {
+			int cy = dy[i] + y;
+			int cx = dx[i] + x;
+			if(cy>=0&&cx>=0&&cy<N&&cx<M&&arr[cy][cx]=='.') {
+				if(!visit[cy][cx]) {
+					visit[cy][cx] = true;
+					dfs(cy,cx,def+1);
+					visit[cy][cx] = false;
+				}
+			}
+		}
+	}
+	private static void draw() {
+		// TODO Auto-generated method stub
+		for(int i=0;i<N;i++) {
+			for(int k=0;k<M;k++) {
+				if(arr[i][k]=='+') {
+					result[i][k] = '+';
+				}else if(arr[i][k]=='.'&&!visit[i][k]) {
+					result[i][k] = '@';
+				}else {
+					result[i][k] = '.';
+				}
+			}
+		}
 	}
 }
